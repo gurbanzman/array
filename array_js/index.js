8c2fc2d7feef7bd67f7c27f25caf4c8f08d7c17e
Murad Gurbanzada <gurbanzadamurad@gmail.com>
2023-10-09 14:56:16 +0400

    array

---
+++ array_js/index.js
@@ -0,0 +1,86 @@
+// array task - 1 
+
+// function array_Clone(arr){
+//    let newArr = [...arr];
+//    return newArr;
+// }
+
+// console.log(array_Clone([1, 2, 4, 0]));
+
+// console.log(array_Clone([1, 2, [4, 0]]));
+
+// array task - 2
+
+// function sliceArray(arr,n){
+//    let sliceArr = arr.slice(0,n);
+//    console.log(sliceArr);
+
+// }
+
+// sliceArray([7, 9, 0, -2],3);
+
+//array task - 3
+
+// function joinString(arr){
+//    console.log(`"${arr.join(" ")}"`);
+// }
+
+// joinString(["Red","Green","White","Black"]);
+
+
+//array task - 4
+
+// let arr1 = [3,'a', 'a', 'a', 2, 3, 'a', 3,'a',2, 4, 9, 3 ];
+
+// let same = 1;
+// let m = 0;
+// let rptElement;
+
+// for(let i = 0; i < arr1.length; i++){
+//    for(let j = i; j < arr1.length; j++){
+//       if(arr1[i] == arr1[j]){
+//          m++;
+//       }
+//       if(rptElement < m){
+//          same = m;
+//          rptElement = arr1[i];
+//       }
+//    }
+//    m = 0;
+// }
+
+
+//task - 5
+
+// let arr = [1,2,3,4,5];
+// let sum = 0;
+// let hasil = 1;
+// for(let i of arr){
+//    sum += i;
+//    hasil *= i;
+// }
+// console.log(sum, hasil);
+
+
+//task - 6
+
+// function dublicateArray(arr){
+//    let emptyArr = [];
+//    for(let i = 0; i < arr.length; i++){
+//       if(!emptyArr.includes(arr[i])) {
+//          emptyArr.push(arr[i]);
+//       }
+//    }
+//    return emptyArr;
+// }
+
+// console.log(emptyArr);
+// dublicateArray([1, 2, -2, 4, 5, 4 ,7, 8, 7 , 7, 71, 3, 6]);
+
+
+//task - 7
+
+// function removeElement(arr, num){
+//    let index = arr.indexOf(num);
+// }
+
